18779e10c5248ffbf1f332f73c6ef989b09043fd
prokushev <yuri.prokushev@gmail.com>
2023-05-08 15:45:17 +0700

    CP/M emulator enabled.

--- a.cmd
+++
@@ -1,7 +0,0 @@
-echo "# radio-86rk" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/prokushev/radio-86rk.git
-git push -u origin main
