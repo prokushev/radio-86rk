ceb31a904d2ca1ba2408c4313766653a44ac1845
prokushev <yuri.prokushev@gmail.com>
2023-05-14 23:05:18 +0700

    Fix or RAMTOP. Parts of MONITOR now in ROM-disk

--- make.cmd
+++ make.cmd
@@ -1,6 +1,8 @@
 asw -lU mon580-306-vt52.asm > mon580-306-vt52.lst
 p2bin mon580-306-vt52.p bios.rom
-asw -lUa mon580-2-rom.asm > mon580-2-rom.lst
+asw -lUa -D STUB=1 mon580-2-rom.asm > mon580-2-rom-1.lst
 p2bin mon580-2-rom.p romdisk.rom
-asw -lU mon580-2.asm > mon580-2.lst
+asw -lUa mon580-2.asm > mon580-2.lst
 p2bin mon580-2.p radiorom.rom
+asw -lUa -D STUB=0 mon580-2-rom.asm > mon580-2-rom-2.lst
+p2bin mon580-2-rom.p romdisk.rom
