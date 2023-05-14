2176034567668ccf0071b88d175b2d49e7339d4f
prokushev <yuri.prokushev@gmail.com>
2023-05-14 10:37:34 +0700

    PrintString and MSGCPM now preserve BC (now backspace processing works fine)

--- make.cmd
+++ make.cmd
@@ -1,3 +1,6 @@
 asw -lU mon580-306-vt52.asm > mon580-306-vt52.lst
 p2bin mon580-306-vt52.p bios.rom
-
+asw -lUa mon580-2-rom.asm > mon580-2-rom.lst
+p2bin mon580-2-rom.p romdisk.rom
+asw -lU mon580-2.asm > mon580-2.lst
+p2bin mon580-2.p radiorom.rom
