4f4fed9c9b26ef7012c67469d2efd6f784266e63
prokushev <yuri.prokushev@gmail.com>
2023-05-17 09:31:44 +0700

    More 16kb version changes.

--- make.cmd
+++ make.cmd
@@ -1,10 +1,12 @@
 asw -lU -D BASE=07600H mon580-306-vt52.asm > mon580-1-32.lst
-p2bin mon580-306-vt52.p bios32k.rom
+p2bin mon580-306-vt52.p bios.rom
 asw -lU -D BASE=03600H mon580-306-vt52.asm > mon580-mon580-1-16.lst
 p2bin mon580-306-vt52.p bios16k.rom
 asw -lUa -D STUB=1 mon580-2-rom.asm > mon580-2-rom-1.lst
 p2bin mon580-2-rom.p romdisk.rom
-asw -lUa mon580-2.asm > mon580-2.lst
+asw -lUa -D BASE=07600H mon580-2.asm > mon580-2-32.lst
 p2bin mon580-2.p radiorom.rom
+asw -lUa -D BASE=03600H mon580-2.asm > mon580-2-16.lst
+p2bin mon580-2.p radiorom16k.rom
 asw -lUa -D STUB=0 mon580-2-rom.asm > mon580-2-rom-2.lst
 p2bin mon580-2-rom.p romdisk.rom
