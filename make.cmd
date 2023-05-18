7161d4a334e25e025c9d53fbbe0fa6f77cf01a97
prokushev <yuri.prokushev@gmail.com>
2023-05-18 21:28:57 +0700

    Read/Write constant sysvar now as in original. Default command buffer size is 16bytes now. ROM-disk for 16kb MONITOR added. Transfer directive screenshot added.

--- make.cmd
+++ make.cmd
@@ -1,12 +1,19 @@
 asw -lU -D BASE=07600H mon580-306-vt52.asm > mon580-1-32.lst
 p2bin mon580-306-vt52.p bios.rom
+
 asw -lU -D BASE=03600H mon580-306-vt52.asm > mon580-1-16.lst
 p2bin mon580-306-vt52.p bios16k.rom
-asw -lUa -D STUB=1 mon580-2-rom.asm > mon580-2-rom-1.lst
+
+asw -lUa -D BASE=07600H -D STUB=1 mon580-2-rom.asm > mon580-2-32-rom-1.lst
 p2bin mon580-2-rom.p romdisk.rom
 asw -lUa -D BASE=07600H mon580-2.asm > mon580-2-32.lst
 p2bin mon580-2.p radiorom.rom
+asw -lUa -D -D BASE=07600H STUB=0 mon580-2-rom.asm > mon580-2-32-rom-2.lst
+p2bin mon580-2-rom.p romdisk.rom
+
+asw -lUa -D BASE=03600H -D STUB=1 mon580-2-rom.asm > mon580-2-16-rom-1.lst
+p2bin mon580-2-rom.p romdisk16k.rom
 asw -lUa -D BASE=03600H mon580-2.asm > mon580-2-16.lst
 p2bin mon580-2.p radiorom16k.rom
-asw -lUa -D STUB=0 mon580-2-rom.asm > mon580-2-rom-2.lst
-p2bin mon580-2-rom.p romdisk.rom
+asw -lUa -D -D BASE=03600H STUB=0 mon580-2-rom.asm > mon580-2-16-rom-2.lst
+p2bin mon580-2-rom.p romdisk16k.rom
