773a092c1742959fc1e4bd921817bd7fbac493bf
prokushev <yuri.prokushev@gmail.com>
2023-05-19 14:01:54 +0700

    Carry flag now set for InputDirective if buffer not empty (Original MONITOR compatibility). Used by some programs.

--- make.cmd
+++ make.cmd
@@ -11,7 +11,7 @@ asw -lUa -D BASE=07600H -D STUB=1 mon580-2-rom.asm > mon580-2-32-rom-1.lst
 p2bin mon580-2-rom.p romdisk.rom
 asw -lUa -D BASE=07600H mon580-2.asm > mon580-2-32.lst
 p2bin mon580-2.p radiorom.rom
-asw -lUa -D -D BASE=07600H STUB=0 mon580-2-rom.asm > mon580-2-32-rom-2.lst
+asw -lUa -D BASE=07600H -D STUB=0 mon580-2-rom.asm > mon580-2-32-rom-2.lst
 p2bin mon580-2-rom.p romdisk.rom
 
 rem MONITOR 2.00 16KB
@@ -19,5 +19,5 @@ asw -lUa -D BASE=03600H -D STUB=1 mon580-2-rom.asm > mon580-2-16-rom-1.lst
 p2bin mon580-2-rom.p romdisk16k.rom
 asw -lUa -D BASE=03600H mon580-2.asm > mon580-2-16.lst
 p2bin mon580-2.p radiorom16k.rom
-asw -lUa -D -D BASE=03600H STUB=0 mon580-2-rom.asm > mon580-2-16-rom-2.lst
+asw -lUa -D BASE=03600H -D STUB=0 mon580-2-rom.asm > mon580-2-16-rom-2.lst
 p2bin mon580-2-rom.p romdisk16k.rom
