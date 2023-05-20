c036d7f78eb7df857675d0206d6ca126ac937dd9
prokushev <yuri.prokushev@gmail.com>
2023-05-20 22:02:36 +0700

    Added versions for Emu80

--- make.cmd
+++ make.cmd
@@ -21,3 +21,7 @@ asw -lUa -D BASE=03600H mon580-2.asm > mon580-2-16.lst
 p2bin mon580-2.p radiorom16k.rom
 asw -lUa -D BASE=03600H -D STUB=0 mon580-2-rom.asm > mon580-2-16-rom-2.lst
 p2bin mon580-2-rom.p romdisk16k.rom
+
+rem Emu80 versions
+copy romdisk.rom romdisk.bin
+copy radiorom.rom rk86.rom
