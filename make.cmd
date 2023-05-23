84c0925687eb40716212810988d870c86a135f4e
prokushev <yuri.prokushev@gmail.com>
2023-05-23 17:07:04 +0700

    echo output disabled during compilation

--- make.cmd
+++ make.cmd
@@ -1,14 +1,15 @@
-rem MONITOR 1.20 32KB
+@echo off
+echo MONITOR 1.20 32KB
 asw -lU -D BASE=07600H mon580-1.asm > mon580-1-32.lst
 p2bin mon580-1.p roms\1.20-32\b2m\bios.rom
 p2bin mon580-1.p roms\1.20-32\emu80\rk86.rom
 
-rem MONITOR 1.20 16KB
+echo MONITOR 1.20 16KB
 asw -lU -D BASE=03600H mon580-1.asm > mon580-1-16.lst
 p2bin mon580-1.p roms\1.20-16\b2m\bios.rom
 p2bin mon580-1.p roms\1.20-16\emu80\rk86.rom
 
-rem MONITOR 2.00 32KB
+echo MONITOR 2.00 32KB
 asw -lUa -D BASE=07600H -D STUB=1 mon580-2-rom.asm > mon580-2-32-rom-1.lst
 p2bin mon580-2-rom.p roms\2.00-32\b2m\romdisk.rom
 p2bin mon580-2-rom.p roms\2.00-32\emu80\romdisk.bin
@@ -19,7 +20,7 @@ asw -lUa -D BASE=07600H -D STUB=0 mon580-2-rom.asm > mon580-2-32-rom-2.lst
 p2bin mon580-2-rom.p roms\2.00-32\b2m\romdisk.rom
 p2bin mon580-2-rom.p roms\2.00-32\emu80\romdisk.bin
 
-rem MONITOR 2.00 16KB
+echo MONITOR 2.00 16KB
 asw -lUa -D BASE=03600H -D STUB=1 mon580-2-rom.asm > mon580-2-16-rom-1.lst
 p2bin mon580-2-rom.p roms\2.00-16\b2m\romdisk.rom
 p2bin mon580-2-rom.p roms\2.00-16\emu80\romdisk.bin
