091ea3b0149c23362c3c6f37d8237aaffaf227d5
prokushev <yuri.prokushev@gmail.com>
2023-05-23 17:04:20 +0700

    binaries sorted by dirs

--- make.cmd
+++ make.cmd
@@ -1,27 +1,32 @@
 rem MONITOR 1.20 32KB
 asw -lU -D BASE=07600H mon580-1.asm > mon580-1-32.lst
-p2bin mon580-1.p bios.rom
+p2bin mon580-1.p roms\1.20-32\b2m\bios.rom
+p2bin mon580-1.p roms\1.20-32\emu80\rk86.rom
 
 rem MONITOR 1.20 16KB
 asw -lU -D BASE=03600H mon580-1.asm > mon580-1-16.lst
-p2bin mon580-1.p bios16k.rom
+p2bin mon580-1.p roms\1.20-16\b2m\bios.rom
+p2bin mon580-1.p roms\1.20-16\emu80\rk86.rom
 
 rem MONITOR 2.00 32KB
 asw -lUa -D BASE=07600H -D STUB=1 mon580-2-rom.asm > mon580-2-32-rom-1.lst
-p2bin mon580-2-rom.p romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-32\b2m\romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-32\emu80\romdisk.bin
 asw -lUa -D BASE=07600H mon580-2.asm > mon580-2-32.lst
-p2bin mon580-2.p radiorom.rom
+p2bin mon580-2.p roms\2.00-32\b2m\radiorom.rom
+p2bin mon580-2.p roms\2.00-32\emu80\rk86.rom
 asw -lUa -D BASE=07600H -D STUB=0 mon580-2-rom.asm > mon580-2-32-rom-2.lst
-p2bin mon580-2-rom.p romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-32\b2m\romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-32\emu80\romdisk.bin
 
 rem MONITOR 2.00 16KB
 asw -lUa -D BASE=03600H -D STUB=1 mon580-2-rom.asm > mon580-2-16-rom-1.lst
-p2bin mon580-2-rom.p romdisk16k.rom
+p2bin mon580-2-rom.p roms\2.00-16\b2m\romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-16\emu80\romdisk.bin
 asw -lUa -D BASE=03600H mon580-2.asm > mon580-2-16.lst
-p2bin mon580-2.p radiorom16k.rom
+p2bin mon580-2.p roms\2.00-16\b2m\radiorom.rom
+p2bin mon580-2.p roms\2.00-16\emu80\rk86.rom
 asw -lUa -D BASE=03600H -D STUB=0 mon580-2-rom.asm > mon580-2-16-rom-2.lst
-p2bin mon580-2-rom.p romdisk16k.rom
+p2bin mon580-2-rom.p roms\2.00-16\b2m\romdisk.rom
+p2bin mon580-2-rom.p roms\2.00-16\emu80\romdisk.bin
 
-rem Emu80 versions
-copy romdisk.rom romdisk.bin
-copy radiorom.rom rk86.rom
