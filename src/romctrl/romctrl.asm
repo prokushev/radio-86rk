80dc6b2a5ccb7eb585eaa2c36ec8d44cadb48f2d
prokushev <yuri.prokushev@gmail.com>
2023-05-30 11:54:27 +0700

    ROM-disk control program now build as separate program. Added radiorom.rom image for Bashkiria-2M for MONITOR 1.20.

---
+++ src/romctrl/romctrl.asm
@@ -0,0 +1,172 @@
+; ═══════════════════════════════════════════════════════════════════════
+; ПРОГРАММА УПРАВЛЕНИЯ ROM-DISK/32K ДЛЯ КОМПЬЮТЕРА 
+; "РАДИО-86РК" С OБЪEMOM ОЗУ ПОЛЬЗОВАТЕЛЯ 16К/32К.
+; ПРОГРАММА УПРАВЛЕНИЯ ЗАФИКСИРОВАНА В ПЗУ 
+; ПО АДРЕСАМ 7E00H-7FFFH. УКАЗАННУЮ ОБЛАСТЬ ПЗУ 
+; ЗАПРЕЩЕНО ИСПОЛЬЗОВАТЬ ПОД ROM-DISK. 
+; ПРОГРАММА ИЗ ПЗУ В ОЗУ ПЕРЕНОСИТСЯ ЗАГРУЗЧИКОМ, 
+; (В MONITORE) ПО ДИРЕКТИВЕ "U" или "R" И ЗАНИМАЕТ 
+; ВЕРХНИЕ АДРЕСА ОЗУ, НАЧИНАЯ С 7400Н.
+; ═══════════════════════════════════════════════════════════════════════
+
+	CPU		8080
+	Z80SYNTAX	EXCLUSIVE
+
+; ───────────────────────────────────────────────────────────────────────
+; Адреса системных вызовов
+; ───────────────────────────────────────────────────────────────────────
+
+	INCLUDE	"syscalls.inc"
+
+	ORG	BASE-0200H
+
+	LD	HL, SO1
+	CALL	PrintString
+	LD	B, 0FFH
+	CALL	SEARCHS
+
+	LD	C, '>'
+	CALL	PrintCharFromC
+	CALL	InputSymbol
+	LD	C, A
+	CALL	PrintCharFromC
+	CP	03h
+	JP	Z,WarmBoot
+	SUB	30H
+	LD	B, A
+	LD	HL, FUNC+1
+	LD	(HL), EXECN & 0ffh
+	INC	HL
+	LD	(HL), EXECN >> 8
+	CALL	SEARCHS
+	JP	WarmBoot
+
+SEARCHS:
+	LD	HL, 0800H		; Начало ROM-диска
+	LD	C, L			; LD C, 0
+SEARCH:
+	PUSH	HL			; (1)
+	LD	DE, (8+2+2)-1
+	ADD	HL, DE
+	EX	HL, DE
+	POP	HL			; (1)
+
+	PUSH	HL			; (2)
+	
+	PUSH	BC			; (3)
+
+	LD	BC, T
+	PUSH	BC			; (4)
+	CALL	ReadROM
+	POP	HL			; (4)
+
+	POP	BC			; (3)
+
+	LD	A, (HL)
+	CP	0FFH
+
+	POP	DE			; (2)
+	
+	RET	Z
+
+FUNC:	CALL	PRINTN
+
+	PUSH	DE			; (5)
+;---------------------
+	LD	HL, (T+8+2)
+	LD	A, L			; высчитываем начало следующей записи
+	OR	A			; оканчивается на ноль
+	JP	Z, SKIP
+	OR	0fh
+	LD	L, A
+	INC	HL
+SKIP:
+	LD	DE, 10h
+	ADD	HL, DE
+;---------------------
+	POP	DE			; (5)
+	
+	ADD	HL, DE
+	INC	C
+	JP	SEARCH
+
+PRINTN:
+	PUSH	HL
+	PUSH	BC
+	PUSH	DE
+
+	LD	A, C			; Печатаем порядковый номер
+	ADD	A, 30H
+	LD	C, A
+	CALL	PrintCharFromC
+	LD	C, ' '
+	CALL	PrintCharFromC
+
+	LD	B, 8
+	LD	HL, T
+PLOOP:	LD	A, (HL)			; Печатаем имя
+	LD	C, A
+	CALL	PrintCharFromC
+	INC	HL
+	DEC	B
+	JP	NZ, PLOOP
+
+	INC	HL
+	LD	C, ' '			; Печатаем стартовый адрес
+	CALL	PrintCharFromC
+	LD	A, (HL)
+	CALL	PrintHexByte
+	DEC	HL
+	LD	A, (HL)
+	CALL	PrintHexByte
+
+	INC	HL
+	INC	HL
+	INC	HL
+	LD	C, ' '			; Печатаем размер
+	CALL	PrintCharFromC
+	LD	A, (HL)
+	CALL	PrintHexByte
+	DEC	HL
+	LD	A, (HL)
+	CALL	PrintHexByte
+
+	LD	HL, SO2			; Печатаем перевод строки
+	CALL	PrintString
+	POP	DE
+	POP	BC
+	POP	HL
+	RET
+
+EXECN:
+	LD	A, C
+	SUB	B
+	RET	NZ
+	
+	LD	HL, 010H
+	ADD	HL, DE		; DE - это адрес на ROM-диске
+	EX	DE, HL
+	
+	LD	SP, T+8		; ИСПОЛЬЗУЯ СТЕК,
+	POP	BC		; start
+	POP	HL		; size
+
+	ADD	HL, DE
+	EX	DE, HL
+
+	PUSH	BC		; Читаем в ОЗУ
+	CALL    ReadROM
+	POP	HL
+
+	JP	(HL)          	;И ЗАПУСТИТЬ ПРОГРАММУ.
+
+T:	DB	8+2+2 DUP 0			;
+
+SO1:	DB 	0AH,0DH,"*ROM-DISK/32K* V3.0-23"
+	DB 	0AH,0AH,0DH,"DIRECTORY:"
+SO2:	DB	0AH,0DH,0
+
+	DB	BASE-3-$ DUP (0FFH)
+
+	; Вход по директиве Z
+	JP	BASE-0200H
